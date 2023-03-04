ee97abe7be5d2522772e5e6845b5eb2b5b03841e
Alex Soca <52253824+a-soca@users.noreply.github.com>
2023-03-04 23:35:10 +0000

    Add files via upload

--- basicFilterResponseGenerator.m
+++ basicFilterResponseGenerator.m
@@ -7,13 +7,23 @@ switch filterType % Depending on the number the user enters, the code corrospond
         hd(n~=0) = 2*fc*(sin(n(n~=0)*wc)/(n(n~=0)*wc)); % Generates the basic filter response array when the value for discrete time index is not 0
         hd(n==0) = 2*fc; % Generates the basic filter response value when the discrete time index is 0
     case 2 % If 2 is selected, the following code will execute
-        % Apply HPF
+        % High pass filter
+        hd(n~=0) = -2*fc*(sin(n(n~=0)*wc)/(n(n~=0)*wc)); % Generates the basic filter response array when the value for discrete time index is not 0
+        hd(n==0) = 1-2*fc; % Generates the basic filter response value when the discrete time index is 0
     case 3 % If 3 is selected, the following code will execute
-        % Prompt user to enter bandwidth
-        % Apply BPF
+        % Band pass filter
+        findCutoff(); % Finds the lower and upper cutoff frequencies around the central selected cutoff frequency
+        hd(n~=0) = 2*fc2*(sin(n(n~=0)*wc)/(n(n~=0)*wc))-2*fc1*(sin(n(n~=0)*wc)/(n(n~=0)*wc)); % Generates the basic filter response array when the value for discrete time index is not 0
+        hd(n==0) = 2*(fc2-fc1); % Generates the basic filter response value when the discrete time index is 0
     case 4 % If 4 is selected, the following code will execute
-        % Prompt user to enter bandwidth
-        % Apply BSF
+        % Band stop filter
+        findCutoff(); % Finds the lower and upper cutoff frequencies around the central selected cutoff frequency
+        hd(n~=0) = 2*fc1*(sin(n(n~=0)*wc)/(n(n~=0)*wc))-2*fc2*(sin(n(n~=0)*wc)/(n(n~=0)*wc)); % Generates the basic filter response array when the value for discrete time index is not 0
+        hd(n==0) = 1-2*(fc2-fc1); % Generates the basic filter response value when the discrete time index is 0
 end
 
-
+function [fc1, fc2] = findCutoff()
+    bandwidth = input("Please enter a bandwidth : ");
+    fc1 = (fc-bandwidth/fs)/2;
+    fc2 = (fc+bandwidth/fs)/2;
+end
