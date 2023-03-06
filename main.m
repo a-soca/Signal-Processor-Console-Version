62cee0e73dc7af6db1282769c5fcab00a5fbabbd
Alex Soca <52253824+a-soca@users.noreply.github.com>
2023-03-06 18:21:26 +0000

    Add files via upload

--- main.m
+++ main.m
@@ -4,9 +4,15 @@ clf; % Clears plotted figures
 
 importAudio;
 
-ws = input("Please enter a window size (This number must be odd) : "); % Prompts the user to input a window size
+ws = NaN; % Initialises ws to a value which is not valid so that the while loop is entered
+while rem(ws,2) == 0 % Loops while ws is not odd
+    ws = input("Please enter a window size (This number must be odd) : "); % Prompts the user to input a window size
+end % Ends while loop
 
-fcUser = input("Please enter a cutoff frequency : "); % Prompts the user to input a cutoff frequency
+fcUser = NaN; % Initialises fcUser to a value which is not valid so that the while loop is entered
+while fcUser < 0 || fcUser > 20000 % Loops while ws is not between 0 and 20000
+    fcUser = input("Please enter a cutoff frequency [0-20000] : "); % Prompts the user to input a cutoff frequency
+end % Ends while loop
 
 windowFunctionGenerator;
 basicFilterResponseGenerator;
--- menuValidation.m
+++ menuValidation.m
@@ -1,7 +1,7 @@
 function [valid] = menuValidation(validInputs, userInput)
 % Checks user input for menu selections
 %   Takes a vector of valid inputs as well as the user input, returns 1 or 
-%   0 if the user input is in the array
+%   0 if the user input is in the vector
     if ismember(userInput, validInputs)
         valid = 1;
     else
