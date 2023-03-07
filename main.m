287c6914b933345801a6ca99166d0e48863bc454
Alex Soca <52253824+a-soca@users.noreply.github.com>
2023-03-07 21:11:54 +0000

    Add files via upload

--- main.m
+++ main.m
@@ -6,11 +6,13 @@ importAudio;
 
 while true % Loops until valid input is provided
     try % Attempts to convert user input to an integer
-        ws = int(input("Please enter a window size (This number must be odd) : ")); % Prompts the user to input a window size
-        if ~rem(ws,2) % if the number input is odd, the following code will execute
-            break % Exits the while loop as the process was successful
-        else % If the number input is even, the following code will execute
-            fprintf("Number must be an odd. Please try again.\n"); % Prints an error message
+        ws = str2double(input("Please enter a window size (This number must be odd) : ", 's')); % Prompts the user to input a window size
+        if rem(ws,2) ~= 0 && ~isnan(ws) % if the number input is an odd integer, the following code will execute
+            break % Exits the while loop as the process was successful and the user input fulfilled requirements
+        elseif rem(ws,2) == 0 % If the number input is even, the following code will execute
+            fprintf("Number must be odd. Please try again.\n"); % Prints an error message
+        else % If the user input was a letter, symbol or not a number then the following will execute
+            fprintf("Window size must be a number. Please try again.\n"); % Prints an error message
         end % ends the if statement
     catch % If the process fails, the following code will execute
         fprintf("Window size must be an odd integer. Please try again.\n"); % Prints an error message
