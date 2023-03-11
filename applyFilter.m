5fc795656ca1178f250c0a4e89469e8f573e1702
Alex Soca <52253824+a-soca@users.noreply.github.com>
2023-03-11 21:15:28 +0000

    Add files via upload

--- applyFilter.m
+++ applyFilter.m
@@ -1,8 +1,17 @@
 %yFinal = conv(h, y(1:end,1)); % Performs convolution on the unprocessed audio file and the filter coefficient vector
 
-
-yFinal = zeros(size(y));
-for i = 1:length(y)-ws
-    yFinal(i:i+ws) = yFinal(i:i+ws)+h(1:end,2).*y(i:i+ws,1);
-end
+%yFinal = zeros(size(y));
+%y = [y zeros(ws)]; % padding unfiltered signal
+%for i = 1:length(y)-ws
+%    yFinal(i:i+ws-1) = yFinal(i:i+ws-1)+h(1:end,2).*y(i:i+ws-1,1);
+%end
 % Currently this only works for single channel audio
+
+
+
+yFinal = zeros(1, length(y));
+for n = ws:length(y)
+    for k = 1:ws
+        yFinal(n) = yFinal(n) + h(k) * y(n-k+1);
+    end
+end
--- filterCoefficientGenerator.m
+++ filterCoefficientGenerator.m
@@ -1,3 +1,3 @@
 h = w.*hd; % Generates the filter coefficient array from the window function and basic filter response
 
-% swap w and hd?
+h = h./sum(h); % Normalises filter coefficients (experimental)
