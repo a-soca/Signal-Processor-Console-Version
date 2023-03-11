700acf2f6cd8d06cbdcf1842786684665a41c08c
Alex Soca <52253824+a-soca@users.noreply.github.com>
2023-03-11 20:06:07 +0000

    Add files via upload

--- applyFilter.m
+++ applyFilter.m
@@ -1,3 +1,8 @@
-yFinal = conv(h, y(1:end,1)); % Performs convolution on the unprocessed audio file and the filter coefficient vector
+%yFinal = conv(h, y(1:end,1)); % Performs convolution on the unprocessed audio file and the filter coefficient vector
 
+
+yFinal = zeros(size(y));
+for i = 1:length(y)-ws
+    yFinal(i:i+ws) = yFinal(i:i+ws)+h(1:end,2).*y(i:i+ws,1);
+end
 % Currently this only works for single channel audio
