181dfd46e3269883aca589872be9afa3a2c72b13
Alex Soca <52253824+a-soca@users.noreply.github.com>
2023-03-14 17:14:40 +0000

    Add files via upload

--- applyFilter.m
+++ applyFilter.m
@@ -1,21 +1,4 @@
 % Currently this only works for single channel audio
-
-yFinal = zeros(1, length(y));
-
-% add multi channel
-intermediate = h.*y;
-for i=1:length(yFinal)
-    yFinal(i) = sum(intermediate(i,1:end));
-end
-
+yFinal = sum(h.*y,2);
 size(yFinal)
-
-
-
-%     \/WORKING\/
-%for i = ws:length(y)    
-%    for k = 1:ws
-%        yFinal(i) = yFinal(i) + h(k) * y(i-k+1);
-%    end
-%end
 
