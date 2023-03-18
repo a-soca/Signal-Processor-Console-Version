85d293c2f32a2f4cf7469a49afc18ddffca7ef80
Alex Soca <52253824+a-soca@users.noreply.github.com>
2023-03-18 20:02:11 +0000

    Delete menuValidation.m

--- menuValidation.m
+++
@@ -1,6 +0,0 @@
-function [valid] = menuValidation(validInputs, userInput)
-% Checks user input for menu selections
-%   Takes a vector of valid inputs as well as the user input, returns 1 or 
-%   0 if the user input is in the vector
-valid = ismember(userInput, validInputs);
-end
