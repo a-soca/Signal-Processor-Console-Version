1e6aa94ec7a4686d3a47d4699f7897dc3cd506de
Alex Soca <52253824+a-soca@users.noreply.github.com>
2023-03-18 20:03:30 +0000

    Add files via upload

--- exportAudio.m
+++ exportAudio.m
@@ -1,1 +1,1 @@
-audiowrite("processed.wav", yFinal, fs); % Writes the array of amplitudes to an audio file
+audiowrite("Processed.wav", yFinal, fs); % Writes the array of amplitudes to an audio file
