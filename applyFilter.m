2789bfc2eba702e9f7bb2803848437aac5aa9597
Alex Soca <52253824+a-soca@users.noreply.github.com>
2023-03-18 19:30:24 +0000

    Add files via upload

--- applyFilter.m
+++ applyFilter.m
@@ -1,18 +1,23 @@
 % Currently this only works for single channel audio
 
-yFinal = zeros(1,length(y));
+yFinal = zeros(1,length(y)); 
+
+%     \/WORKING With matrix method\/
+for i = 1:length(y)-ws+1
+    yFinal(i:i+ws-1) = yFinal(i:i+ws-1) + h.*y(i+ws-1);
+end
 
 %     \/WORKING\/
-for i = ws:length(y)
-    for k = 1:ws
-        yFinal(i) = yFinal(i) + h(k) * y(i-k+1);
-    end
-end
+%for i = ws:length(y)
+%    for j = 1:ws
+%        yFinal(i) = yFinal(i) + h(j) * y(i-j+1);
+%    end
+%end
 
 %     \/WORKING ALT\/
 %for i = 1:length(y)-ws
-%    for k = 1:ws
-%        yFinal(i) = yFinal(i) + h(k) * y(i+k-1);
+%    for j = 1:ws
+%        yFinal(i) = yFinal(i) + h(j) * y(i+j-1);
 %    end
 %end
 
