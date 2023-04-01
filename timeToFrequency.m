ca5727752bbb9a514840f309185c587c3187c36f
Alex Soca <52253824+a-soca@users.noreply.github.com>
2023-04-01 08:39:57 +0100

    Add files via upload

--- timeToFrequency.m
+++ timeToFrequency.m
@@ -5,8 +5,8 @@ for c = 1:channelNum % For the number of channels of audio, the following code w
     yAmpProcessed(:,c) = abs(fft(yProcessed(:, c))); % Finds the real numbers in the discrete fourier transform of the processed data and stores them in an array
 end % Ends the for loop
 
-yAmpInit = yAmpInit(1:floor(length(yAmpInit)/2))*2; % Halves the length of the array to remove the symmetrical points and doubles the amplitudes to redistribute the energy from the removed points
+yAmpInit = yAmpInit(1:floor(length(yAmpInit)/2),channelNum)*2; % Halves the length of the array to remove the symmetrical points and doubles the amplitudes to redistribute the energy from the removed points
 
-yAmpProcessed = yAmpProcessed(1:floor(length(yAmpProcessed)/2))*2; % Halves the length of the array to remove the symmetrical points and doubles the amplitudes to redistribute the energy from the removed points
+yAmpProcessed = yAmpProcessed(1:floor(length(yAmpProcessed)/2),channelNum)*2; % Halves the length of the array to remove the symmetrical points and doubles the amplitudes to redistribute the energy from the removed points
 
 xFreq = 0:(fs/length(y)):(fs/2-fs/length(y)); % Creates an array of frequencies
